dda66bf39040435a7454563014b22e9925df70e9
Renz Carillo <renzcarillo32@gmail.com>
2021-09-01 17:12:22 +0800

    :white_check_mark: added route file that compiles all of the frontend pages

---
+++ client/src/Routes.tsx
@@ -0,0 +1,17 @@
+/* 
+compilation of all frontend pages 
+*/
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function Routes() {
+    return (
+        <Router>
+            <Route>
+                {/* frontend page */}
+            </Route>
+        </Router>
+    )
+}
+
+export default Routes;
