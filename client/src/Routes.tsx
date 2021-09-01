d70932cbd79c1c416864c83afcc259692d7563a6
Renz Carillo <renzcarillo32@gmail.com>
2021-09-01 17:21:06 +0800

    :white_check_mark: updated frontend routes file

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -3,13 +3,19 @@ compilation of all frontend pages
 */
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './auth/PrivateRoute';
 
 function Routes() {
     return (
         <Router>
-            <Route>
-                {/* frontend page */}
-            </Route>
+            <Switch>
+                <Route path = "">
+                    {/* frontend page | landing page */}
+                </Route>
+                <PrivateRoute>
+                    {/* authorized page | newsfeed */}
+                </PrivateRoute>
+            </Switch>
         </Router>
     )
 }
