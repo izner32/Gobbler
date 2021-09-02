4bfb6290bb23566926f462b7876ba030817e715c
Renz Carillo <renzcarillo32@gmail.com>
2021-09-02 18:34:59 +0800

    :bug: fixed routing issues with nextjs

--- client/src/auth/PrivateRoute.tsx
+++ client/src/auth/PrivateRoute.tsx
@@ -1,19 +1,27 @@
 /* 
 - creating a private route that returns a route which could only be accessed if you're an authorized user 
 - if there is no value for user meaning no one is logged in, redirect them back to login page
+- use this higher order component to wrap private pages 
 */
-import { Redirect, Route } from 'react-router-dom';
+
+import { useRouter } from "next/router";
 import { useUser } from "./useUser";
 
-const PrivateRoute = props => {
-    // validate if user exists
+// we gonna wrap private pages with this higher order component
+const PrivateRoute = Component => {
+    // returning a function so we could grab its props 
+    const Auth = props => {
+    
     const user = useUser();
+    const router = useRouter();
 
+    // validate if user exists
     if(!user){
-        return <Redirect to="/login" />
+        return router.push("/login");
     }
-
-    return <Route {...props} />
+    // return the page if user is authorized
+    return <Component {...props} />
+    }
 }
 
 export default PrivateRoute;
