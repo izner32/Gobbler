149df9ba6631c5576a795855e75b0f2c72bcfa30
Renz Carillo <renzcarillo32@gmail.com>
2021-09-03 15:22:52 +0800

    :lipstick: added turkey color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
       fontFamily: {
         Poppins: ['Poppins'],
       },
+      colors: {
+        Turkey: {
+          '50': '#fef7f6', 
+          '100': '#fdefed', 
+          '200': '#fad7d2', 
+          '300': '#f7bfb7', 
+          '400': '#f18e80', 
+          '500': '#eb5e4a', 
+          '600': '#d45543', 
+          '700': '#b04738', 
+          '800': '#8d382c', 
+          '900': '#732e24',
+        },
+      },
     },
   },
   variants: {
