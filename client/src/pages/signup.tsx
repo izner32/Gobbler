7918eca63c5bfcc8aed12d794d81c5d8465dc6c9
Renz Carillo <renzcarillo32@gmail.com>
2021-09-04 13:34:42 +0800

    :lipstick: fixed margins, hover to the buttons and input fields on sign up page

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -39,31 +39,31 @@ function Signup() {
                     <img className ="md:w-[30%] lg:w-72 absolute bottom-0 ml-[20%]" src="Turkey.svg" alt="Girl in a jacket"></img>
                 </div>
                 <div className="col-span-12 md:col-span-7 ">
-                    <div className="m-28 md:ml-16 md:mr-16 ">
+                    <div className="m-28 md:ml-16 md:mr-16 lg:ml-20 lg:mr-20 xl:ml-24 xl:mr-24">
                         <h2 className="font-Poppins text-2xl md:text-2xl lg:text-3xl xl:text-4xl mb-6">Sign Up</h2>
                         <hr className="mb-6"></hr>
                         <div className="mb-6">
-                            <label className="block font">Email</label>
+                            <label className="block mb-2">Email</label>
                             <input
                                 type="email"
-                                className="block w-auto"
+                                className="w-full p-2 appearance-none border-2 border-white rounded focus:outline-none focus:bg-white focus:border-Turkey-500"
                                 value={emailValue}
                                 onChange={e => setEmailValue(e.target.value)}
                                 placeholder="someone@gmail.com" />
                         </div>
                         <div className="mb-6">
-                            <label className="block font">Username</label>
+                            <label className="block mb-2">Username</label>
                             <input
                                 type="text"
-                                className="block w-auto"
-                                value={emailValue}
+                                className="w-full p-2 appearance-none border-2 border-white rounded focus:outline-none focus:bg-white focus:border-Turkey-500"
+                                value={usernameValue}
                                 onChange={e => setEmailValue(e.target.value)}
                                 placeholder="username" />
                         </div>
                         <div className="mb-6">
-                            <label className="block">Password</label>
+                            <label className="block mb-2">Password</label>
                             <input
-                                className=""
+                                className="w-full p-2 appearance-none border-2 border-white rounded focus:outline-none focus:bg-white focus:border-Turkey-500"
                                 type="password"
                                 value={passwordValue}
                                 onChange={e => setPasswordValue(e.target.value)}
@@ -71,11 +71,11 @@ function Signup() {
                         </div>
                         <div className="flex flex-col md:flex-row">
                             <button
-                                className="text-sm md:text-sm lg:text-base xl:text-lg p-2 pr-6 pl-6 md:mr-6 mb-6 md:-mb-0 text-white bg-Turkey-500 rounded-md"
+                                className="text-sm md:text-sm lg:text-base xl:text-lg p-2 pr-6 pl-6 md:mr-6 mb-6 md:-mb-0 text-white bg-Turkey-500 rounded-md hover:bg-Turkey-600"
                                 disabled={!emailValue || !passwordValue} // disable if the value of these two is blank 
                                 onClick={onSignUpClicked}>Sign&nbsp;Up</button> 
                             <button 
-                                className="text-sm md:text-sm lg:text-base xl:text-lg p-2 pr-6 pl-6 md:mr-6 text-Turkey-500 hover:text-white hover:bg-Turkey-500 hover:rounded-md"
+                                className="text-sm md:text-sm lg:text-base xl:text-lg p-2 pr-6 pl-6 md:mr-6 text-Turkey-500 hover:text-Turkey-800"
                                 onClick={() => router.push('/login')}>I already have an account</button>
                         </div>
                     </div>
