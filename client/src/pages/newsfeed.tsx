9e51efddf7b7934ab8b4246afc389a09c86b7130
Renz Carillo <renzcarillo32@gmail.com>
2021-09-08 19:22:56 +0800

    :white_check_mark: added heart and message svg files

--- client/src/pages/newsfeed.tsx
+++ client/src/pages/newsfeed.tsx
@@ -16,24 +16,35 @@ function newsfeed() {
                 <a className="" href="/js/">JavaScript</a> 
                 <a className="" href="/jquery/">jQuery</a>
             </nav>
-            <div className="grid grid-cols-6">
-                <div>
-                    <img className="">
-                    </img>
-                </div>
-                <div className="relative">
+            <div className="">
+                <div className="relative border rounded-lg w-96 shadow-md ml-4">
                     <img className ="absolute bottom-3 left-3" src="Email.svg" alt="Email"></img>
                     <input
                         type="email"
-                        className="w-full p-2 appearance-none border-2 border-white rounded focus:outline-none focus:bg-white focus:border-Turkey-500 pl-12"
+                        className="block whitespace-normal break-words w-64 p-2 appearance-none border-2 border-white rounded focus:outline-none focus:bg-white focus:border-Turkey-0 pl-12"
                         value={postValue}
                         onChange={e => setPostValue(e.target.value)}
                         placeholder="What's on your mind?" />
+                    <button
+                        className="absolute bottom-2.5 right-3 pl-5 pr-5 text-white bg-Turkey-500 rounded-md hover:bg-Turkey-600"
+                        disabled={!postValue} 
+                        onClick={onPostClicked}>Gobble
+                    </button> 
                 </div>
-                <button
-                    className="pl-5 pr-5 text-white bg-Turkey-500 rounded-md hover:bg-Turkey-600"
-                    disabled={!postValue} 
-                    onClick={onPostClicked}>Gobble</button> 
+
+                {/* this is how posts shall look like */}
+                <div>
+                    <div>
+                        <img></img>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    <div>
+                    <hr></hr>
+                    <p></p>
+                    
+                    </div>
+                </div>
             </div>
         </div>
     )
